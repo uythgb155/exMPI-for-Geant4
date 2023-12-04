043b11b63e74ea8d3f5bf57a4256dcebee9bdfbe
uythgb155 <842436763@qq.com>
2023-12-04 16:01:01 +0800

    “多线程程序-可视化完成”

--- exMPI01.cc
+++ exMPI01.cc
@@ -68,6 +68,14 @@ int main(int argc, char** argv)
   prompt += "[40;31m(%s)[40;36m[%/][00;30m:";
   session-> SetPrompt(prompt);
 
+  // Detect interactive mode (if no arguments) and define UI session
+  //
+  G4UIExecutive* ui = 0;
+  if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+  }
+
+
   // --------------------------------------------------------------------
   // user application setting
   // --------------------------------------------------------------------
@@ -88,7 +96,22 @@ int main(int argc, char** argv)
   G4VisExecutive* visManager = new G4VisExecutive;
   visManager-> Initialize();
   G4cout << G4endl;
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
+  // Process macro or start UI session
+  //
+  if ( ! ui ) {
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else {
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
   // --------------------------------------------------------------------
   // ready for go
   // MPIsession treats both interactive and batch modes.
